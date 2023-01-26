596a3ac4dea2c165a4e329e8b42b2a8c9fb873f4
Mahmoud Mostafa <mahmoud309mostafa@gmail.com>
2023-01-26 16:42:30 +0200

    add user api

--- BackEnd/routes/users.js
+++ BackEnd/routes/users.js
@@ -1,5 +1,24 @@
 import express from "express";
+import {
+  deleteUser,
+  getAllUser,
+  getOneUser,
+  updateUser,
+} from "../controller/user.js";
+import { verifyUser, verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+// Update User Info
+router.put("/:id", verifyUser, updateUser);
+
+// Delete User
+router.delete("/:id", verifyUser, deleteUser);
+
+// Get User Info
+router.get("/:id", verifyUser, getOneUser);
+
+// Get All Users
+router.get("/", verifyAdmin, getAllUser);
 
 export default router;
---
+++ BackEnd/utils/verifyToken.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+import { createError } from "./error.js";
+
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token;
+  if (!token) return next(createError(401, "You arn't authenticated"));
+
+  jwt.verify(token, process.env.JWT, (err, user) => {
+    if (err) return next(createError(403, "Token isn't valid!"));
+    req.user = user;
+    next();
+  });
+};
+
+export const verifyUser = (req, res, next) => {
+  verifyToken(req, res, next, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      return next(createError(403, "You arn't authorized!"));
+    }
+  });
+};
+
+export const verifyAdmin = (req, res, next) => {
+  verifyToken(req, res, next, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      return next(createError(403, "You arn't authorized!"));
+    }
+  });
+};
