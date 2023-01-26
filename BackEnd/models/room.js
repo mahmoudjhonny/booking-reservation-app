21f86ea4987ba6c58432dd1e2c8df679a30ab710
Mahmoud Mostafa <mahmoud309mostafa@gmail.com>
2023-01-26 16:42:11 +0200

    Add room api

---
+++ BackEnd/models/room.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const roomSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    maxPeople: {
+      type: Number,
+      required: true,
+    },
+    desc: {
+      type: String,
+      default: true,
+    },
+    roomNumbers: [{ number: Number, unavalibleDates: { type: [Date] } }],
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Room", roomSchema);
--- BackEnd/routes/rooms.js
+++ BackEnd/routes/rooms.js
@@ -1,5 +1,29 @@
 import express from "express";
+import {
+  createRoom,
+  deleteRoom,
+  getAllRoom,
+  getOneRoom,
+  updateRoom,
+} from "../controller/room.js";
+
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+// post new hotel
+router.post("/:hotelId", verifyAdmin, createRoom);
+
+// Edit an existing hotel
+router.put("/:id", verifyAdmin, updateRoom);
+
+// Delete an existing hotel
+router.delete("/:id/:hotelId", verifyAdmin, deleteRoom);
+
+// get one hotel by id
+router.get("/:id", getOneRoom);
+
+// get all existing hotel
+router.get("/", getAllRoom);
 
 export default router;
