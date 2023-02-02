e828d37b76f9907aa237ebfc7862ecf8b1e7790e
Mahmoud Mostafa <mahmoud309mostafa@gmail.com>
2023-02-02 14:55:09 +0200

    Add Hotel Page

--- FrontEnd/src/Pages/Hotel/Hotel.js
+++ FrontEnd/src/Pages/Hotel/Hotel.js
@@ -4,86 +4,64 @@ import Header from "../../Components/Header/Header";
 import MailList from "../../Components/MailList/MailList";
 import Footer from "../../Components/Footer/Footer";
 import { LocationOn } from "@mui/icons-material";
+import useFetch from "../../Api_Call/useFetch";
+import { useLocation } from "react-router-dom";
 
 const Hotel = () => {
-  const photos = [
-    {
-      src: "https://gttp.imgix.net/202499/x/0/101659a-hb-a-001-jpg?auto=format%2Ccompress&crop=faces%2Cedges%2Ccenter&bg=%23fff&q=35&fit=crop",
-    },
-    {
-      src: "https://gttp.imgix.net/202499/x/0/101659a-hb-a-001-jpg?auto=format%2Ccompress&crop=faces%2Cedges%2Ccenter&bg=%23fff&q=35&fit=crop",
-    },
-    {
-      src: "https://gttp.imgix.net/202499/x/0/101659a-hb-a-001-jpg?auto=format%2Ccompress&crop=faces%2Cedges%2Ccenter&bg=%23fff&q=35&fit=crop",
-    },
-    {
-      src: "https://gttp.imgix.net/202499/x/0/101659a-hb-a-001-jpg?auto=format%2Ccompress&crop=faces%2Cedges%2Ccenter&bg=%23fff&q=35&fit=crop",
-    },
-    {
-      src: "https://gttp.imgix.net/202499/x/0/101659a-hb-a-001-jpg?auto=format%2Ccompress&crop=faces%2Cedges%2Ccenter&bg=%23fff&q=35&fit=crop",
-    },
-    {
-      src: "https://gttp.imgix.net/202499/x/0/101659a-hb-a-001-jpg?auto=format%2Ccompress&crop=faces%2Cedges%2Ccenter&bg=%23fff&q=35&fit=crop",
-    },
-  ];
+  const location = useLocation();
+  const id = location.pathname.split("/")[2];
+  const { loading, data } = useFetch(
+    `http://localhost:8080/api/hotels/find/${id}`
+  );
+  console.log(data);
   return (
     <div>
       <Navbar />
       <Header type="list" />
-      <div className="hotelContainer">
-        <div className="hotelWrapper">
-          <button className="bookNow">Reserve or Book Now!</button>
-          <h1 className="hotelTitle">Grand Hotel</h1>
-          <div className="hotelAddress">
-            <LocationOn />
-            <span>Elton St 125 New York</span>
-          </div>
-          <span className="hotelDistance">
-            Excellent location - 500m from center
-          </span>
-          <span className="hotelPriceHighlight">
-            Book a stay over $114 at this property and get a free airport taxi
-          </span>
-          <div className="hotelImages">
-            {photos.map((img) => (
-              <div className="hotelImgWrapper">
-                <img src={img.src} alt="hotel" className="hotelImg" />
+      {loading ? (
+        "Loading...."
+      ) : (
+        <div className="hotelContainer">
+          <div className="hotelWrapper">
+            <button className="bookNow">Reserve or Book Now!</button>
+            <h1 className="hotelTitle">{data.name}</h1>
+            <div className="hotelAddress">
+              <LocationOn />
+              <span>{data.adress}</span>
+            </div>
+            <span className="hotelDistance">
+              Excellent location - {data.distance} m from center
+            </span>
+            <span className="hotelPriceHighlight">
+              Book a stay over ${data.cheapstPrice} at this property and get a
+              free airport taxi
+            </span>
+            <div className="hotelImages">
+              {data.photos?.map((img, i) => (
+                <div className="hotelImgWrapper" key={i}>
+                  <img src={img} alt="hotel" className="hotelImg" />
+                </div>
+              ))}
+            </div>
+            <div className="hotelDetails">
+              <div className="hotelDetailsText">
+                <h1 className="hotelTitle">{data.name}</h1>
+                <p className="hotelDesc">{data.desc}</p>
               </div>
-            ))}
-          </div>
-          <div className="hotelDetails">
-            <div className="hotelDetailsText">
-              <h1 className="hotelTitle">Stay in the heart of Krakow</h1>
-              <p className="hotelDesc">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book. It has
-                survived not only five centuries, but also the leap into
-                electronic typesetting, remaining essentially unchanged. It was
-                popularised in the 1960s with the release of Letraset sheets
-                containing Lorem Ipsum passages, and more recently with desktop
-                publishing software like Aldus PageMaker including versions of
-                Lorem Ipsum.
-              </p>
-            </div>
-            <div className="hotelDetailsPrice">
-              <h1>Perfect for a 9-nights stay!</h1>
-              <span>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s.
-              </span>
-              <h2>
-                <b>$945</b> (9 nights)
-              </h2>
-              <button>Reserve or Book now!</button>
+              <div className="hotelDetailsPrice">
+                <h1>Perfect for a 9-nights stay!</h1>
+                <span>{data.desc}</span>
+                <h2>
+                  <b>${data.cheapstPrice}</b> (9 nights)
+                </h2>
+                <button>Reserve or Book now!</button>
+              </div>
             </div>
           </div>
+          <MailList />
+          <Footer />
         </div>
-        <MailList />
-        <Footer />
-      </div>
+      )}
     </div>
   );
 };
